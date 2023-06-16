780f26b7829a764d8c7f39d4c6877176be342181
djvon <vonengineers@gmail.com>
2023-06-16 12:05:53 -0500

    Implement ag-grid tree component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,55 @@
 "use client";
+import { useRef, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+// data
+import RowData from "./data/gridData";
 
 export default function Home() {
-  const rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 },
-  ];
+  const gridRef = useRef();
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+    };
+  }, []);
+  const autoGroupColumnDef = useMemo(() => {
+    return {
+      headerName: "Organisation Hierarchy",
+      minWidth: 300,
+      cellRendererParams: {
+        suppressCount: true,
+      },
+    };
+  }, []);
+  const getDataPath = useMemo(() => {
+    return (data) => {
+      return data.orgHierarchy;
+    };
+  }, []);
 
   const columnDefs = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
+    // we're using the auto group column by default!
+    { field: "jobTitle" },
+    { field: "employmentType" },
   ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+        <AgGridReact
+          ref={gridRef}
+          rowData={RowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          autoGroupColumnDef={autoGroupColumnDef}
+          treeData={true}
+          animateRows={true}
+          groupDefaultExpanded={-1}
+          getDataPath={getDataPath}
+        ></AgGridReact>
       </div>
     </main>
   );
